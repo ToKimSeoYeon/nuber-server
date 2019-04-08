c2ec970787b29bc0ba6a87f7fa0651b0c02e03b0
ToKimSeoYeon <rlagmlwnd3@daum.net>
2019-04-08 13:13:17 +0900

    1.19

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -1,6 +1,7 @@
-import { verificationTarget } from "src/types/type";
+import { verificationTarget } from "src/types/types";
 import {
   BaseEntity,
+  BeforeInsert,
   Column,
   CreateDateColumn,
   Entity,
@@ -8,11 +9,14 @@ import {
   UpdateDateColumn
 } from "typeorm";
 
+const PHONE = "PHONE";
+const EMAIL = "EMAIL";
+
 @Entity()
 class Verification extends BaseEntity {
   @PrimaryGeneratedColumn() id: number;
 
-  @Column({ type: "text", enum: ["PHONE", "EMAIL"] })
+  @Column({ type: "text", enum: [PHONE, EMAIL] })
   target: verificationTarget;
 
   @Column({ type: "text" })
@@ -26,5 +30,16 @@ class Verification extends BaseEntity {
 
   @CreateDateColumn() createdAt: string;
   @UpdateDateColumn() updatedAt: string;
+
+  @BeforeInsert()
+  createKey(): void {
+    if (this.target === PHONE) {
+      this.key = Math.floor(Math.random() * 100000).toString();
+    } else if (this.target === EMAIL) {
+      this.key = Math.random()
+        .toString(36)
+        .substr(2);
+    }
+  }
 }
 export default Verification;
